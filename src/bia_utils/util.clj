f4485fc3e5d71a9ca35d3ec2269edd562e3f5343
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-26 15:39:50 +0400

    add add-one-k-mer

--- src/bia_utils/util.clj
+++ src/bia_utils/util.clj
@@ -8,9 +8,13 @@
   (let [end (+ i k)]
     (<= end len)))
 
-(defn add-one-k-mer [i text k acc]
+(defn get-one-k-mer [i text k]
   (let [end (+ i k)
-        k-mer (subs text i end)
+        k-mer (subs text i end)]
+    k-mer))
+
+(defn add-one-k-mer [i text k acc]
+  (let [k-mer (get-one-k-mer i text k)
         new-acc (update-in acc [k-mer] (fnil inc 0))]
     new-acc))
 
